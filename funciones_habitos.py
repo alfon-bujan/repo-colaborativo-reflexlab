aeb387a0e6a88318e0cbf3f32e39cba34e80c366
nunezmatilde <nunezm@udesa.edu.ar>
2026-03-23 08:53:27 -0300

    Modifique la funcion habitos

--- funciones_habitos.py
+++ funciones_habitos.py
@@ -14,5 +14,40 @@
 
 
 
-# --------------------------MIRANDA-----------------------------------------
+
+
+
+
+
+
+
+
+
+
+# --------------------------MATILDE-----------------------------------------
+
+def analizar_habitos(lista):
+    """
+    La funcion recibe una lista de actividades y devuelve un diccionario con la cantidad de veces que aparece cada una. 
+    
+    Parameters
+    ----------
+    lista : lista de actividades
+    
+    Returns 
+    -------
+    diccionario : diccionario en donde las claves son las actividades y el valor es la cantidad de veces que aparecen. 
+
+    """
+    diccionario={}
+    
+    for i in lista:
+        if i not in diccionario:
+            diccionario[i]=1
+            
+        else:
+            diccionario[i]+=1
+    
+    return diccionario 
+    
 
